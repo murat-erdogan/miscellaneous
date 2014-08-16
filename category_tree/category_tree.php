6aa0349e02f05d85ba61cfd18448bd97db472c56
murat-erdogan <murat-erdogan@users.noreply.github.com>
2014-08-16 21:59:04 +0300

    Update category_tree.php

--- category_tree/category_tree.php
+++ category_tree/category_tree.php
@@ -1,6 +1,9 @@
 <?php
 
-//MySQL table should contain id, name, parent columns.
+/*
+MySQL table should contain id, name, parent columns.
+I used recursive function to walk through category array.
+*/
 
 $db = new mysqli('localhost', 'root', '', 'test');
 if($db->connect_errno > 0){
@@ -10,15 +13,15 @@ if($db->connect_errno > 0){
 $sql ="SELECT * FROM `categories`";
 $result=$db->query($sql);
 while($row = $result->fetch_assoc()){
-     $arr[]=$row;
+     $arr[] = $row;
 }
 
-function get_tree($arr,$id){
+function get_tree($arr, $id){
 	$arr2 = array();
     
     foreach($arr as $key => $value){
-        if($value['parent']==$id){
-            $arr2[$value['id']]=$value;
+        if($value['parent'] == $id){
+            $arr2[$value['id']] = $value;
             $arr2[$value['id']]['children'] = get_tree($arr,$value['id']);
         }
     }
@@ -26,6 +29,6 @@ function get_tree($arr,$id){
 }
 
 echo "<pre>";
-print_r(get_tree($arr,0));
+print_r(get_tree($arr, 0));
 
 ?>
