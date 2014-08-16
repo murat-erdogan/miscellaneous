2ddcb90ada1fb4d7c7d9f097c1363d0a2d2a61df
murat-erdogan <murat-erdogan@users.noreply.github.com>
2014-08-16 20:52:05 +0300

    Create weighted_random.php

---
+++ weighted_random/weighted_random.php
@@ -0,0 +1,20 @@
+<?php
+function randomize_weighted($numbers) {
+    $total = 0;
+    foreach ($numbers as $number => $weight) {
+        $total += $weight;
+        $distribution[$number] = $total;
+    }
+    $rand = mt_rand(0, $total - 1);
+    foreach ($distribution as $number => $weights) {
+        if ($rand < $weights) { return $number; }
+    }
+}
+
+$items = array('ford' => 100,
+				'bmw'  => 50,
+				'audi'  => 25);
+
+$item = randomize_weighted($items);
+echo $item;
+?>
