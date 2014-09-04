e6de3d4a232d509b601d2b986b90f6085b608141
murat-erdogan <murat-erdogan@users.noreply.github.com>
2014-09-04 11:59:01 +0300

    Update example.php

--- ezsql_connect/example.php
+++ ezsql_connect/example.php
@@ -9,6 +9,9 @@ $db_host = "localhost";
 
 $db = new ezSQL_mysqli($db_user, $db_password, $db_name, $db_host);
 
+//This line is crucial especially for Turkish characters. Table charset must set to utf8_general_ci.
+$db->query("SET NAMES 'utf8'");
+
 $current_time = $db->get_var("SELECT " . $db->sysdate());
 echo $current_time;
 
