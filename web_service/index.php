082cb3993a166aaa2aa1c00cdd8756c29b3da437
murat-erdogan <murat-erdogan@users.noreply.github.com>
2014-09-12 16:39:12 +0300

    Update index.php

--- web_service/index.php
+++ web_service/index.php
@@ -11,25 +11,28 @@
 	$db = new ezSQL_mysqli($db_user, $db_password, $db_name, $db_host);
 	$db->query("SET NAMES 'utf8'");
 	
-	$username = $db->escape($_POST['username']);
-	$password = $db->escape($_POST['password']);
-	$function = $_POST['function'];
-	
-	$auth = $db->get_var("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
-	
-	if ($auth <> 0) {
-		if ($function == 'get_table') {
-			$data->success = 1;
-			$data->data = $db->get_results("SELECT * FROM test1");
-			echo json_encode($data);
-		}else if ($function == 'get_table2'){
-			$data->success = 1;
-			$data->data = $db->get_results("SELECT * FROM test2");
+	if ($_POST) {
+		$post_data = json_decode($_POST['data']);
+		$username = $db->escape($post_data->username);
+		$password = $db->escape($post_data->password);
+		$function = $post_data->function->name;
+		
+		$auth = $db->get_var("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
+		
+		if ($auth <> 0) {
+			if ($function == 'get_table') {
+				$data->success = 1;
+				$data->data = $db->get_results("SELECT * FROM test1");
+				echo json_encode($data);
+			}else if ($function == 'get_table2') {
+				$data->success = 1;
+				$data->data = $db->get_results("SELECT * FROM test2");
+				echo json_encode($data);
+			}
+		}else {
+			$data->success = 0;
+			$data->error = 'authentication error';
 			echo json_encode($data);
 		}
-	}else {
-		$data->success = 0;
-		$data->error = 'authentication error';
-		echo json_encode($data);
 	}
 ?>
