9da4debeceeaa6e0e42a1715b7a240ebe1b508c5
Murat Erdogan <murat-erdogan@users.noreply.github.com>
2016-04-30 22:45:32 +0100

    Added function

--- python/knapsack.py
+++ python/knapsack.py
@@ -1,17 +1,24 @@
-items = [3, 8, 5, 1, 4]
-values = [27, 10, 12, 37, 15]
-capacity = 17
+def knapsack(items, values, capacity):
+    result = []
+    table = [[0 for i in range(capacity+1)] for i in range(len(items)+1)]
 
-table = [[0 for i in range(capacity+1)] for i in range(len(items)+1)]
+    for i in range(1, len(items)+1):
+        for c in range(1, capacity+1):
+            if items[i-1] > c:
+                table[i][c] = table[i - 1][c]
+            else:
+                table[i][c] = max(values[i - 1] + table[i - 1][c - items[i - 1]], table[i - 1][c])
 
-for i in range(1, len(items)+1):
-    for c in range(1, capacity+1):
-        if items[i-1] > c:
-            table[i][c] = table[i - 1][c]
-        else:
-            table[i][c] = max(values[i - 1] + table[i - 1][c - items[i - 1]], table[i - 1][c])
+    for i in range(len(items), 0, -1):
+        if table[i][capacity] != table[i-1][capacity]:
+            result.append(items[i-1])
+            capacity -= items[i-1]
+    return table[-1][-1], result
 
-for i in table:
-    print(i)
+v = [27, 10, 12, 37, 15]
+w = [3, 8, 5, 1, 4]
+W = 17
 
-print("result", table[-1][-1])
+r = knapsack(w, v, W)
+print("result: ", r[0])
+print("items: ", r[1])
